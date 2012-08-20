ead2cc67108b9e7f1d7107192adcc81e297ddc53
Florian Bruhin <me@the-compiler.org>
2012-08-20 17:36:19 +0200

    Clean up and improve sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 case $1 in
-	hd) df -h | grep $(mount | grep "on / " | cut -d " " -f 1) | sed 's/[^ ]* *\([^ ]*\) *\([^ ]*\).*/\2\/\1/;s/G//g;s/\.0//g' ;;
-	irc) netstat | grep -c ircd ;;
-	hilights) tail -n 0 -f ~/.irssihilights ;;
-	uptime) uptime | sed "s/.* up \([^,]*\),.*user.*/\1/;s/  / /;s/ days,/d/" ;;
-	procs) ps axue | grep -vE "^USER|grep|ps" | wc -l ;;
-	mem) free -m | head -n 2 | tail -n 1 | sed 's/[^ ]* *\([^ ]*\) *\([^ ]*\).*/\2\/\1/;s/G//g' ;;
-	who) echo \($(whoami)\) $(who | cut -d " " -f 1 | sort | uniq | sed ':a;N;$!ba;s/\n/, /g') ;;
+	hd) df -h / | awk 'NR == 2 { gsub("G", ""); gsub("\\.0", ""); print $3 "/" $2 }' ;;
+	uptime) uptime | sed "s/.* up \([^,]*\).*/\1/;s/  / /;s/ days,/d/" ;;
+	procs) ps axe --no-headers | wc -l ;;
+	mem) free -m | awk 'NR==2 { print $3 "/" $2}' ;;
+	load) uptime | sed 's/.*load average: //;s/,//g' ;;
 esac
-exit 0
